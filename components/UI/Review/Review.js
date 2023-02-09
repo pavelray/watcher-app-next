b82f3eaa68fa84ce8545be5425b61b3573e1b3a8
Pavel Ray <pray@falabella.cl>
2023-02-09 13:32:05 +0530

    Review show more and show less added

--- components/UI/Review/Review.js
+++ components/UI/Review/Review.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { style } from "./Review.styles";
 
 const Review = ({ review }) => {
@@ -9,6 +9,35 @@ const Review = ({ review }) => {
     created_at,
   } = review;
   const createdDate = new Date(created_at).toLocaleDateString();
+
+  const checkForOverflow = () => {
+    const expandableElements = document.querySelectorAll("[data-expandable]");
+    expandableElements.forEach((expandableElement) => {
+      if (expandableElement.classList.contains("expanded")) return;
+      const expandableText =
+        expandableElement.querySelector("[data-expand-text]");
+      const overflowing =
+        expandableText.scrollHeight > expandableText.clientHeight+1;
+      expandableElement.dataset.overflow = overflowing;
+    });
+  };
+
+  const toggleText = (e) => {
+    const expandableElement = e.target.closest("[data-expandable]");
+    expandableElement.classList.toggle("expanded");
+    setExpandButtonText(e.target);
+  };
+
+  const setExpandButtonText = (button) => {
+    const expandableElement = button.closest("[data-expandable]");
+    const expanded = expandableElement.classList.contains("expanded");
+    button.innerText = expanded ? "Read Less" : "Read More";
+  };
+
+  useEffect(() => {
+    checkForOverflow();
+  }, []);
+
   return (
     <div className="review">
       <div className="review-header">
@@ -17,10 +46,16 @@ const Review = ({ review }) => {
           <label className="rating"> {rating}/10</label>
         </p>
       </div>
-      <p
-        className="review-description"
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
+      <div data-expandable>
+        <p
+          data-expand-text
+          className="review-description"
+          dangerouslySetInnerHTML={{ __html: content }}
+        />
+        <button data-expand-button onClick={toggleText} className='read-more'>
+          Read More
+        </button>
+      </div>
       <p className="review-stats">
         <label className="date">{createdDate}</label>
       </p>
--- components/UI/Review/Review.styles.js
+++ components/UI/Review/Review.styles.js
@@ -17,7 +17,7 @@ const style = css`
       word-spacing: 1px;
       margin: 2px 0px;
       font-style: italic;
-      white-space: pre-line;    
+      white-space: pre-line;
       word-break: break-all;
     }
     &-author {
@@ -26,7 +26,7 @@ const style = css`
       word-spacing: 5px;
       margin: 5px 0px;
       font-weight: bold;
-      font-family: 'Crete Round', serif;
+      font-family: "Crete Round", serif;
       color: $secondary;
     }
     &-stats {
@@ -34,7 +34,7 @@ const style = css`
       font-weight: bold;
       display: flex;
       flex-direction: column;
-      font-family: 'Crete Round', serif;
+      font-family: "Crete Round", serif;
       color: $secondary;
 
       .date {
@@ -42,10 +42,60 @@ const style = css`
         font-weight: normal;
         letter-spacing: 1px;
         margin: 5px 0px;
-        font-family: 'Crete Round', serif;
-        color: $secondary
+        font-family: "Crete Round", serif;
+        color: $secondary;
       }
     }
+    .read-more{
+      border: none;
+      background: $background;
+      color: $tertiary;
+      font-weight: 700;
+      letter-spacing: 1px;
+      font-size: 12px;
+      padding: 0;
+     
+      &:hover {
+        cursor: pointer;
+        border-bottom: 1px solid $tertiary;
+      }
+    }
+  }
+
+  [data-expandable] [data-expand-text] {
+    --line-height: 1.5;
+    --lines-to-show: 3;
+    position: relative;
+    box-sizing: border-box;
+    line-height: var(--line-height);
+    overflow: hidden;
+    height: calc(var(--line-height) * var(--lines-to-show) * 1em);
+
+    &::before {
+      content: "";
+      position: absolute;
+      height: 100%;
+      width: 100%;
+      background: linear-gradient(to bottom, transparent, #fff);
+    }
+  }
+
+  [data-overflow="false"] [data-expand-text] {
+    height: initial;
+    &::before {
+      background: initial;
+    }
+  }
+
+  [data-expandable].expanded [data-expand-text] {
+    height: initial;
+    &::before {
+      background: initial;
+    }
+  }
+
+  [data-overflow="false"] [data-expand-button] {
+    display: none;
   }
 `;
 
