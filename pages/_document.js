64e94cd7588e60533cc256b14238ee1e4c2c73be
Pavel Ray <pray@falabella.cl>
2023-02-16 13:00:10 +0530

    Added gtm tags

--- pages/_document.js
+++ pages/_document.js
@@ -79,21 +79,24 @@ export default function Document() {
         <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
         <meta name="theme-color" content="#ffffff"></meta>
         <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-4JG944BM1B"
-        ></Script>
-        <Script
-          id="google-analytics"
+          id="gtm-tag"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `(function(){window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-4JG944BM1B');})()`,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NCCW446')`,
           }}
         ></Script>
       </Head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NCCW446"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        ></noscript>
         <Main />
         <NextScript />
         <Script
